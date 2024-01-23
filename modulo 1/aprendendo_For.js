91b64d3762edfbf988ee7fcbff48bdf43f7dc244
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2024-01-23 11:03:38 -0300

    Update aprendendo_For.js

--- modulo 1/aprendendo_For.js
+++ modulo 1/aprendendo_For.js
@@ -1,6 +1,5 @@
 const groceryList = ['arroz','feijão','alface','melancia','macarrão'];
 
-/*for(let i = 0; i < groceryList.length; i+=1){
+for(let i = 0; i < groceryList.length; i+=1){
     console.log(groceryList[i]);
-}*/
-console.log(groceryList);
+}
