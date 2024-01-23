85c15cd1ae81cfc81331d0f589980917e336c5a3
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2024-01-23 11:03:09 -0300

    Add files via upload

---
+++ modulo 1/aprendendo_For.js
@@ -0,0 +1,6 @@
+const groceryList = ['arroz','feijão','alface','melancia','macarrão'];
+
+/*for(let i = 0; i < groceryList.length; i+=1){
+    console.log(groceryList[i]);
+}*/
+console.log(groceryList);
