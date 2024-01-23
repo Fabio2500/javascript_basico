c53d44969a16790d7639323513af1fb8ec4ac92a
Fabio Rafael Dias Viana <102665846+Fabio2500@users.noreply.github.com>
2024-01-23 10:55:18 -0300

    Add files via upload

---
+++ modulo 1/arrays.js
@@ -0,0 +1,5 @@
+const gameMenu = ['status','inventary','abilities','perks'];
+const menuPerks = 'força , velocidade , destreza';
+gameMenu.unshift(menuPerks);
+gameMenu.push('config');
+console.log(gameMenu);
